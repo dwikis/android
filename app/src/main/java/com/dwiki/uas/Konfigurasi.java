df210eecd30b25f582afd6bf319cfd86481b5ac9
dwikis <supmarhernanda@gmail.com>
2019-07-13 00:06:58 +0700

    uas android

---
+++ app/src/main/java/com/dwiki/uas/Konfigurasi.java
@@ -0,0 +1,23 @@
+package com.dwiki.uas;
+
+public class Konfigurasi {
+    public static final String URL_ADD="http://192.168.68.1/android/tambah.php";
+    public static final String URL_GET_ALL="http://192.168.68.1/android/tampil.php";
+
+    //Dibawah ini merupakan Kunci yang akan digunakan untuk mengirim permintaan ke Skrip PHP
+    public static final String KEY_EMP_ID = "id";
+    public static final String KEY_NAMA = "nama";
+    public static final String KEY_NOHP = "nohp"; //desg itu variabel untuk posisi
+    public static final String KEY_ALAMAT = "alamat"; //salary itu variabel untuk gajih
+    public static final String KEY_JK = "jk"; //salary itu variabel untuk gajih
+
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_NOHP = "nohp";
+    public static final String TAG_ALAMAT = "alamat";
+    public static final String TAG_JK = "jk";
+
+    public static final String EMP_ID = "emp_id";
+}
